a858e687f921ea4ebd1e09c71063591813c2a146
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-02 20:41:09 +0545

    setting up main express app

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import todoRoutes from "./routes/todoapp.js";
+
+const app = express();
+const PORT = 3000;
+
+//to get __dirname 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Middleware
+
+//for view (ejs files)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+//for static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", todoRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
