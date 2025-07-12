647afbdc1e3731a4ea0afb7825e2391ef8b2af5a
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-12 19:40:30 +0545

    header and footer file

---
+++ todolist-react/src/components/static/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div id="footer-container">
+      <div id="support">contact</div>
+      <div id="copyrights">
+        &copy; {new Date().getFullYear()} Aashish Chakradhar. All rights
+        reserved.
+      </div>
+    </div>
+  );
+}
+export default Footer;
---
+++ todolist-react/src/components/static/header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <div id="header-container">
+      <div id="header-title">Keeper</div>
+    </div>
+  );
+}
+export default Header;
