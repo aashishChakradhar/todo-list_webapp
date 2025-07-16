8f9c1a70529d6776be265da1622767037182d8db
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-16 16:27:01 +0545

    adding disable button function

--- todolist-react/src/components/todo/form.jsx
+++ todolist-react/src/components/todo/form.jsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 function AddNote(props) {
   const [title, setTitle] = useState("");
   const [note, setNote] = useState("");
+  const [errorMsg, setErrorMessage] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (!title.trim() && !note.trim()) return;
+    if (!title.trim() && !note.trim()) {
+      setErrorMessage("Please insert a note!");
+      return;
+    }
     props.onAddNote({ title: title.trim() || "(Untitled)", note: note.trim() });
     setTitle("");
     setNote("");
+    setErrorMessage("");
   }
 
   return (
@@ -21,6 +26,7 @@ function AddNote(props) {
         value={title}
         onChange={(e) => {
           setTitle(e.target.value);
+          setErrorMessage("");
         }}
       />
       <textarea
@@ -30,9 +36,15 @@ function AddNote(props) {
         value={note}
         onChange={(e) => {
           setNote(e.target.value);
+          setErrorMessage("");
         }}
       />
-      <button type="submit" id="note-button" onClick={handleSubmit}>
+      <div id="error-msg">{errorMsg}</div>
+      <button
+        type="submit"
+        id="note-button"
+        disabled={!title.trim() && !note.trim()}
+      >
         Add
       </button>
     </form>
